9075ca01f70ee93805f1afe9e97481ba58a163f7
Laken Harville <rlakenharville@gmail.com>
2022-03-01 15:39:24 -0600

    Initial commit

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ArraysAndLists
 {
     class Program
@@ -27,6 +27,43 @@ namespace ArraysAndLists
              *
              * Try to be creative in your display
              */
+
+            var numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var evens = new List<int>();
+            var odds = new List<int>();
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evens.Add(numbers[i]);
+                }
+                else
+                {
+                    odds.Add(numbers[i]);
+                }
+            }
+            foreach (var item in evens)
+            {
+                Console.WriteLine($"{item} is even, not odd.");
+            }
+            foreach (var item in odds)
+            {
+                Console.WriteLine($"{item} is odd, not even.");
+            }
+
         }
     }
 }
